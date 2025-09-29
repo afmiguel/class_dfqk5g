34a967072bfc6f77a5d42e1211f87d89dd57b069
Afonso Miguel <afonso.miguel@pucpr.br>
2025-09-29 17:49:43 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::thread::sleep;
+use std::thread;
 use std::time::Duration;
 
 fn main() {
     for i in 0..10 {
         println!("I'm the main Thread! Count is {i}");
-        sleep(Duration::from_secs(1));
+        thread::sleep(Duration::from_secs(1));
     }
 }
 
-// Teste
