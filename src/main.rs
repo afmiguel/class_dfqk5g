ff65c8e003769cbb0a02dec062b92768f89a5df2
Afonso Miguel <afonso.miguel@pucpr.br>
2025-09-29 17:52:02 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    thread::spawn(||{
+        for i in 0..20{
+            println!("Sou a thread. Contador = {i}");
+            thread::sleep(Duration::from_millis(300));
+        }
+    });
+
     for i in 0..10 {
         println!("I'm the main Thread! Count is {i}");
         thread::sleep(Duration::from_secs(1));
